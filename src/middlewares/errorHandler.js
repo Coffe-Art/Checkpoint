4aa037e3ccb0939bc8da04171686e8c19e41718d
Coffe-Art <CoffeArt224@gmail.com>
2024-08-06 22:23:27 -0500

    Delete src directory

--- src/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-// src/middleware/errorHandler.js
-const errorHandler = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({ error: err.message || 'Ocurrió un error en el servidor' });
-  };
-  
-  module.exports = errorHandler;
-  
