debef0df0f461e34d9f5ca5b44264c4933b81f4c
Coffe-Art <CoffeArt224@gmail.com>
2024-08-12 17:34:03 -0500

    Add files via upload

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -9,7 +9,5 @@ router.post('/register', authController.register);
 // Ruta para el inicio de sesión de usuarios
 router.post('/login', authController.login);
 
-// Ruta para verificar si el usuario ya existe
-router.post('/checkIfUserExists', authController.checkIfUserExists);
 
 module.exports = router;
