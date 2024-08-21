8f0f810f977c3f59a7c61b34810a72729704066b
Coffe-Art <CoffeArt224@gmail.com>
2024-08-21 15:16:54 -0500

    Add files via upload

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,17 +1,16 @@
-// src/services/emailService.js
 require('dotenv').config();
 const { EmailClient } = require('@azure/communication-email');
 
-// Configura tu conexión con Azure Communication Service
-const connectionString = '6UFo5dwqZrtlXmSuZMFRBjwFNo8YgjMiH5EUNWoD5QEKNctFHKpVJQQJ99AHACULyCps5mg0AAAAAZCSOs7T';
+const connectionString = 'endpoint=https://coffeartemails.unitedstates.communication.azure.com/;accesskey=6UFo5dwqZrtlXmSuZMFRBjwFNo8YgjMiH5EUNWoD5QEKNctFHKpVJQQJ99AHACULyCps5mg0AAAAAZCSOs7T';
 const emailClient = new EmailClient(connectionString);
 
-const sendEmail = async (to, subject, body) => {
+const sendEmail = async (to) => {
     const message = {
-        senderAddress: 'your-email@example.com', // Reemplaza con tu dirección de correo registrada en Azure
+        senderAddress: 'DoNotReply@0b3ff4b4-b1e8-4be9-987d-d149c4a490c9.azurecomm.net',
         content: {
-            subject: subject,
-            plainText: body,
+            subject: 'Correo electrónico de prueba',
+            html: '<html><h1>Hola mundo por correo electrónico.</h1></html>',
+            plainText: 'Hola mundo por correo electrónico',
         },
         recipients: {
             to: [{ email: to }],
