be5390f947454ccb72254eec16533d906560dbba
Coffe Art 224 <CoffeArt224@gmail.com>
2024-08-26 13:49:23 -0500

    Update insumos.js

--- src/models/insumos.js
+++ src/models/insumos.js
@@ -1,26 +1,27 @@
 const db = require('../utils/db');
 
 const Insumo = {
-  create: (Nombre, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, IdUsuario, callback) => {
-    const query = 'CALL CreateInsumo(?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    db.query(query, [Nombre, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, IdUsuario], callback);
+  create: (Nombre, cantidadInsumo, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, codigoEmpresa, callback) => {
+    const query = 'CALL CreateInsumo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(query, [Nombre, cantidadInsumo, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, codigoEmpresa], callback);
   },
   findById: (IdInsumo, callback) => {
     const query = 'CALL ReadInsumo(?)';
     db.query(query, [IdInsumo], callback);
   },
-  update: (IdInsumo, Nombre, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, IdUsuario, callback) => {
-    const query = 'CALL UpdateInsumo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-    db.query(query, [IdInsumo, Nombre, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, IdUsuario], callback);
+  update: (IdInsumo, Nombre, cantidadInsumo, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, codigoEmpresa, callback) => {
+    const query = 'CALL UpdateInsumo(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(query, [IdInsumo, Nombre, cantidadInsumo, precioUnitario, precioPorKilo, descripcion, lugarDeVenta, correoContacto, TelefonoContacto, TipoDeVenta, codigoEmpresa], callback);
   },
   delete: (IdInsumo, callback) => {
     const query = 'CALL DeleteInsumo(?)';
     db.query(query, [IdInsumo], callback);
   },
-  findByEmpresa: (codigoempresa, callback) => {
+  findByEmpresa: (codigoEmpresa, callback) => {
     const query = 'CALL GetInsumosByEmpresa(?)';
-    db.query(query, [codigoempresa], callback);
+    db.query(query, [codigoEmpresa], callback);
   }
 };
 
 module.exports = Insumo;
+
